0276139220c82337ca01ac4f5cbe2a0e8df24ddc
Ghazal <kalhorghazal1378@gmail.com>
2021-03-12 19:34:21 +0000

    Upload New File

---
+++ src/Server.cpp
@@ -0,0 +1,26 @@
+#include "Server.h"
+
+Server::Server(Configuration configuration)
+//: users(configuration.get_users()),
+//command_handler(configuration.get_users())
+{
+}
+
+void Server::start()
+{
+    /**
+     * select.
+     * call command_handler.handle ...
+     */
+}
+
+// #include "Server.h"
+// #include "Configuration.h"
+
+int main()
+{
+    // Configuration configuration();
+    // Server server(configuration);
+    // server.start();
+    return 0;
+}
