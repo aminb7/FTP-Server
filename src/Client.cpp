ca80811053b621fba7397638006b8b4c799e2059
Ghazal <kalhorghazal1378@gmail.com>
2021-03-12 19:33:32 +0000

    Upload New File

---
+++ src/Client.cpp
@@ -0,0 +1,20 @@
+#include "Client.h"
+
+void Client::start()
+{
+    /**
+     * connect to server
+     * loop on:
+     *      receive input command.
+     *      send to server.
+     *      receive output of command from server.
+     *      print output of command.
+     */
+}
+
+int main()
+{
+    // Client client();
+    // client.start();
+    return 0;
+}
