9f09bb0ce5458f7d64e36c336d14985247f249f2
Ghazal <kalhorghazal1378@gmail.com>
2021-03-12 19:33:00 +0000

    Upload New File

---
+++ include/Server.h
@@ -0,0 +1,15 @@
+#include <vector>
+
+#include "Configuration.h"
+
+class Server
+{
+public:
+    Server(Configuration configuration);
+
+    void start();
+
+private:
+    // CommandHandler command_handler;
+    // std::vector<User> users;
+};
