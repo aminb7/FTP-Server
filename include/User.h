a84633d95fe4af956199b34d05c43ad3df23b29f
Ghazal <kalhorghazal1378@gmail.com>
2021-03-12 19:33:13 +0000

    Upload New File

---
+++ include/User.h
@@ -0,0 +1,18 @@
+#ifndef USER_H_
+#define USER_H_
+
+using namespace std;
+
+#include <iostream>
+#include <string>
+
+class User
+{
+public:
+
+private:
+    string name;
+    string password;
+};
+
+#endif
