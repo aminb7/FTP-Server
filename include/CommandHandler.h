6b46aafb51ac1b8d03d0c4ead6b409f56750b845
Ghazal <kalhorghazal1378@gmail.com>
2021-03-22 15:52:19 +0000

    Update CommandHandler.h

--- include/CommandHandler.h
+++ include/CommandHandler.h
@@ -1,11 +1,34 @@
 #ifndef COMMANDHANDLER_H_
 #define COMMANDHANDLER_H_
 
+#include "User.h"
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sys/stat.h>
+//#include <system>
+
+using namespace std;
+
+#define SUCCESS true
+#define FAILURE false
+
 class CommandHandler
 {
 public:
-
+    static CommandHandler* getInstance();
+    bool handleCreateNewFile(string filePath);
+    bool handleCreateNewDirectory(string dirPath);
+    bool handleDeleteDirectory(string dirPath);
+    bool handleDeleteFile(string filePath);
+    string handleGetCurrentDirectory();
 private:
+    CommandHandler();
+    static CommandHandler* instance;
+    vector<User*> users;
+    User* loggedIn;
 
 };
 
